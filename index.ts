7aeedf2c95dc6a31cbca0e0114e6c13becdcc3e7
Geographer <geographer@live.fr>
2023-10-03 14:01:51 +0200

    More comments / explanations

--- index.ts
+++ index.ts
@@ -4,8 +4,11 @@ import { DatabasePool, createPool, sql } from 'slonik'
 dotenv.config()
 
 interface TransactionHistoryEntry {
+  // ISO format is fine
   timestamp: string
   direction: 'in' | 'out'
+  // Different assets have different decimals. We store everything in absolute
+  // value, but we want use decimals in the CSV report.
   volume: string
   symbol: string
 }
